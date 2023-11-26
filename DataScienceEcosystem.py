a3ef24ca6f35696020c1acc6a6531ca00a5c3583
Josteneverfresh <151767301+Josteneverfresh@users.noreply.github.com>
2023-11-26 03:06:03 +0100

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# #  Data Science Tools and Ecosystem
+
+# ## author:
+# Justin Every
+
+# ### In this notebook , data science tools and ecosystem are summarized.
+
+# Objectives:
+# + Create a markdown cell to list data science languages
+# + Create a markdown cell to list data science libraries
+# + Create a markdown cell with a table of Data Science tools
+
+# Some of the popular langauge that data Scientist use are:
+# 1. Python
+# 2. R
+# 3. Julia
+# 
+
+# Some of the popular libraries that data Scientist use are:
+# 1. TensorFlow
+# 2. SciPy
+# 3. NumPy
+# 4. Pandas
+# 5. Matplotlib
+
+# | Data science Tools | 
+# | -------------| 
+# | jupyter lite | 
+# | jupyter notebook | 
+# | github | 
+# | rstudio |
+
+# ### Below are a few examples of evaluating erithmetic expressions in Python
+
+# In[4]:
+
+
+#this is a simple arithmetic expression to multply then add intergers
+(3*4)+5
+
+
+# In[5]:
+
+
+#This will convert 200 minutes to hours
+200/60
+
+
+# In[ ]:
+
+
+
+
